bcf4328172932169d9d4f21149e0ac2b924736d4
Michael Freidkin <freidkin@gmail.com>
2024-04-28 13:20:17 +0300

    Set config portal as non-blocking -
      * to immediately see changes

--- base_clock.ino
+++ base_clock.ino
@@ -1,3 +1,7 @@
+#include <WiFiManager.h>
+#include "utils.h"
+
+extern WiFiManager wifiManager;
 #include "utils.h"
 #include <AsyncTimer.h>
 #include <TimeLib.h>
@@ -11,15 +15,19 @@ void HandleTime()
 {
   if (WiFi.status() == WL_CONNECTED)
   {
+    // Once per minute update epoch from web
     if (second(epoch_utc) == 0)
     {
       epoch_utc = GetEpoch();
       Serial.printf("Update\n");
     }
+    else // Otherwise - update epoch seconds manually
+    {
+      epoch_utc++;
+    }
     Serial.printf("%d %d %02d   ", weekday(epoch_utc), day(epoch_utc), month(epoch_utc));
-    Serial.printf("%02d:%02d:%02d\n", hour(epoch_utc), minute(epoch_utc), second(epoch_utc));
-    // Due run each second - update epoch time
-    epoch_utc++;
+    Serial.printf("%02d:%02d:%02d ", hour(epoch_utc), minute(epoch_utc), second(epoch_utc));
+    Serial.printf(" B%d L%d\n", g_config.brightness, g_config.language);
   }
 }
 
@@ -33,16 +41,26 @@ void setup()
 
   setupWiFi();
 
+  // Initialize time
   epoch_utc = GetEpoch();
+  // Setup each second update time handler
   update_time.setInterval(HandleTime, 1000);
 }
 
 void loop()
 {
-  if (digitalRead(CONFIG_PIN) == LOW)
-  {
-    ConfigPortal();
-    ESP.reset();
-  }
   update_time.handle();
+  // If config portal is stated - just handle it
+  if (wifiManager.getConfigPortalActive())
+  {
+    wifiManager.process();
+  }
+  else // If config porial is requested - start if\t
+  {
+    if (digitalRead(CONFIG_PIN) == LOW)
+    {
+      wifiManager.setConfigPortalBlocking(false);
+      wifiManager.startConfigPortal("Config");
+    }
+  }
 }
