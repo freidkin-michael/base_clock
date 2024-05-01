05ed8cf3059bb02a8417519dafddb99308f6696f
Michael Freidkin <freidkin@gmail.com>
2024-05-01 07:48:45 +0300

    Rework parameters to custom HTML version

--- WiFi.cpp
+++ WiFi.cpp
@@ -2,34 +2,51 @@
 #include "utils.h"
 
 WiFiManager wifiManager;
-WiFiManagerParameter brightnessField("brightness", "Brightness %", "50", 3);
-WiFiManagerParameter languageField("language", "Language", "Eng", 10);
+// Custom HTML for brightness slider and language 
+const char *bufferStr = R"(
+  <br/>
+  <label for='brightness'>Brightness</label>
+  <input type="range" min="1" max="255" value="50" name='brightness'> 
 
+  <br/>
+  <label for='language'>Language</label>
+  <select name="language" id="language" class="button">
+  <option value="0" selected>English</option>
+  <option value="1">Russian</option>
+  <option value="2">Hebrew</option>
+  </select>
+  )";
+
+WiFiManagerParameter custom_html_inputs(bufferStr);
+
+String getParam(String name)
+{
+    // read parameter from server, for custom hmtl input
+    String value = "Unknown";
+    if (wifiManager.server->hasArg(name))
+    {
+        value = wifiManager.server->arg(name);
+    }
+    return value;
+}
 void saveParamCallback()
 {
-    Serial.printf("\nSave Data: B %d\n", atoi(brightnessField.getValue()));
+    String language = getParam("language");
+    String brightness = getParam("brightness");
+    Serial.printf("language: %s\n", language.c_str());
+    Serial.printf("brightness: %s\n", brightness.c_str());
+
     config_t config;
-    config.brightness = atoi(brightnessField.getValue());
-    if (config.brightness < 0 || config.brightness > 100)
-    {
-        // If value not in the range - revert back value and return
-        brightnessField.setValue(String(g_config.brightness).c_str(), 3);
-        return;
-    }
+    config.brightness = atoi(brightness.c_str());
 
-    config.language = atoi(languageField.getValue());
+    config.language = atoi(language.c_str());
 
-    brightnessField.setValue(String(g_config.brightness).c_str(), 3);
-    languageField.setValue(String(g_config.language).c_str(), 10);
     SaveConfig(config);
 }
 
 void setupWiFi()
 {
-    brightnessField.setValue(String(g_config.brightness).c_str(), 3);
-    languageField.setValue(String(g_config.language).c_str(), 10);
-    wifiManager.addParameter(&brightnessField);
-    wifiManager.addParameter(&languageField);
+    wifiManager.addParameter(&custom_html_inputs);
 
     wifiManager.setSaveParamsCallback(saveParamCallback);
     wifiManager.setClass("invert");
