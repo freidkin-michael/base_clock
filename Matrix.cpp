ed64625f48ced5c971d5482036a0891479786500
Michael Freidkin <freidkin@gmail.com>
2024-06-02 15:39:27 +0300

    Add WS2812

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,3 +1,5 @@
+#define MATRIX 0
+#if (MATRIX == 1)
 #include <GyverMAX7219.h>
 
 #define WIDTH 8
@@ -10,7 +12,7 @@ MAX7219<WIDTH, HEIGH, CS_PIN> mtrx;
 
 void MatrixBegin(void)
 {
-    mtrx.begin();
+  mtrx.begin();
 }
 void MatrixPrint(String time, String date)
 {
@@ -21,9 +23,39 @@ void MatrixPrint(String time, String date)
   mtrx.update();
 }
 
-
-
 void MatrixBrightness(uint8_t brightness)
 {
   mtrx.setBright(&brightness);
 }
+#else
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+#define PSTR // Make Arduino Due happy
+#endif
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, D7,
+                                               NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE + NEO_MATRIX_ZIGZAG,
+                                               NEO_GRB + NEO_KHZ800);
+
+void MatrixBegin()
+{
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(2);
+  matrix.setTextColor(matrix.Color(255, 255, 255));
+}
+
+int x = matrix.width();
+int pass = 0;
+
+void MatrixPrint(String time, String date)
+{
+  matrix.fillScreen(0);
+  matrix.setCursor(1, 0);
+  matrix.print(time);
+  matrix.show();
+}
+#endif
